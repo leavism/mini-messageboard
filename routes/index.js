21a2ee2cdf631b04fc1ee55a3142b5b30930e41b
leavism <ghuydang@gmail.com>
2021-01-04 00:04:25 -0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { messages });
+});
+
+module.exports = router;
